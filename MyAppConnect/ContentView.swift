98aa75fb7b113ce249c9602bd7ebfc140dfe0a6b
skr03 <sudarshan.krishnakumar@sky.uk>
2019-12-03 07:41:58 +0000

    Initial Commit

---
+++ MyAppConnect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAppConnect
+//
+//  Created by Vedarth Solutions on 12/3/19.
+//  Copyright © 2019 Sky Plc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
